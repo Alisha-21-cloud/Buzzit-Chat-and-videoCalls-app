c55f3467bdc980cc6c9ea1f26dc012952ac8e9b4
Alisha-21-cloud <ahmadalishasyed@gmail.com>
2025-08-11 12:07:58 +0530

    10 : tanstack query and axios setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,24 @@ import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
 
+import { useQuery } from '@tanstack/react-query';
+import { axiosInstance } from './lib/axios.js';
+
 
 const App = () => {
+
+  const {data} = useQuery({
+    queryKey:["todos"],
+
+    queryFn: async() => {
+      const res = await axiosInstance.get("http://localhost:5001/api/auth/me");
+      return res.data;
+    },
+    retry: false,
+  });
+
+  console.log(data);
+
   return (
     <div className=" h-screen text-5xl" data-theme="night">
       <Routes>
